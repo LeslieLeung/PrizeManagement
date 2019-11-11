665afb50bf5d0d36a5e4bddc9754964a24ae74e4
LeslieLeung <lesily9@gmail.com>
2019-11-11 20:15:16 +0800

    ==

--- src/main/dao/AdminDAO.java
+++ src/main/dao/AdminDAO.java
@@ -1,10 +1,12 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import model.Student;
 
+import java.sql.*;
+
+/**
+ * @author leslie
+ */
 public class AdminDAO {
 
     public AdminDAO() {
@@ -18,6 +20,31 @@ public class AdminDAO {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbd:mysql://localhost:3306/prize_management?characterEncoding=UTF-8",
                 "root","");
+    }
+
+    public boolean validatePassword(Student student) {
+
+        String sql = "SELECT * FROM admin WHERE number = ? AND password = ?";
+        try (Connection c = getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
+
+            System.out.println(student.toString());
+            ps.setString(1, student.number);
+            ps.setString(2, student.password);
+
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println(ps);
+            if (rs.next()) {
+                //正确
+                return true;
+            } else {
+                //错误
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 
